7863c7aeb9e6a9cd20bc4ac7689773bd375504d8
Mayuri <mayurigurav1997@gmail.com>
2024-03-29 14:07:58 +0530

    ErrorBoundrary Added

--- src/components/Users.js
+++ src/components/Users.js
@@ -16,6 +16,11 @@ class Users extends Component{
       more:"Test"
     };
   }
+  componentDidUpdate(){
+    if(this.props.users.length===0){
+      throw new Error("No users provided!!")
+    }
+  }
   toggleUsersHandler(){
      this.setState((curState)=> {
       return {showUsers: !curState.showUsers};
