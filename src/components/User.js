fb9bc39b110869c9dd5f9efa5fc240f8e56da7b4
Mayuri <mayurigurav1997@gmail.com>
2024-03-29 09:26:12 +0530

    git changes

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,10 @@
 import classes from './User.module.css';
 
+class User extends Components{
+     render(){
+      return <li className={classes.user}>{props.name}</li>
+     }
+}
 const User = (props) => {
   return <li className={classes.user}>{props.name}</li>;
 };
