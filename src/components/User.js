6be1e1008f86d6536d59b15b3963ce9ab015bf04
Mayuri <mayurigurav1997@gmail.com>
2024-03-29 09:31:09 +0530

    render method added in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,16 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-class User extends Components{
+class User extends Component{
+    //  constructor(){
+
+    //  }
      render(){
-      return <li className={classes.user}>{props.name}</li>
+      return <li className={classes.user}>{this.props.name}</li>
      }
 }
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
